83b849178a305c11ffe246c3e22495c775bdc426
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2024-04-09 16:33:51 +0700

    Pengumpulan JS-8 Minggu 8

--- Minggu 8/Praktikum 4 Menghias Upload File/form_upload_ajax.php
+++ Minggu 8/Praktikum 4 Menghias Upload File/form_upload_ajax.php
@@ -2,17 +2,18 @@
 <html>
 <head>
     <title>Unggah File Dokumen</title>
-    <link rel="stylesheet" href="upload.css">
 </head>
 <body>
     <div class="upload-form-container">
         <h2>Unggah File Dokumen</h2>
-        <link rel="upload.css">
+        <link rel="stylesheet" href="upload.css">
+        <!-- Membuat form untuk mengunggah file -->
         <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
             <div class="file-input-container">
                 <input type="file" name="file[]" id="file" class="file-input" accept="image/" multiple>
                 <label for="file" class="file-label">Pilih file</label>
             </div>
+            <!-- Tombol unggah file -->
             <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
         </form>
         <div id="status" class="upload-status"></div>
--- Minggu 8/Praktikum 4 Menghias Upload File/upload.js
+++ Minggu 8/Praktikum 4 Menghias Upload File/upload.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
     $('#file').change(function(){
+        // Memeriksa apakah ada file yang dipilih
         if(this.files.length > 0){
+            // Menampilkan tombol unggah 
             $('#upload-button').prop("disabled", false).css('opacity', 1);
         } else {
+            // Menonaktifkan tombol unggah 
             $('#upload-button').prop('disabled', true).css('opacity', 0.5);
         }
     });
 
-
+    //  Membuat kode submit pada form unggah
     $('#upload-form').submit(function(e){
-        e.preventDefault();
+        e.preventDefault(); // Mencegah perilaku default dari submit form
         
-        var formData = new FormData(this);
-
+        var formData = new FormData(this); // Membuat objek FormData dari data form
+        
+        // Mengirim data form menggunakan AJAX
         $.ajax({
             type: 'POST',
             url: 'upload_ajax.php',
@@ -21,9 +25,11 @@ $(document).ready(function(){
             contentType: false,
             processData: false,
             success: function(response){
+                // Menampilkan pada server di elemen dengan id "status"
                 $('#status').html(response);
             },
             error: function(){
+                // Menampilkan pesan kesalahan jika salah upload file
                 $('#status').html('Terjadi kesalahan saat mengunggah file.');
             }
         });     
